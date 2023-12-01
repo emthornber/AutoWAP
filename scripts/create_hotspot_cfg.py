e27fe1ae6986906ba3621167e8328d6306bd0a07
Mark Thornber <mark.thornber@gmail.com>
2023-12-01 09:06:26 +0000

    create hotspot configuration file via script from attribute definition

--- scripts/create_hotspot_cfg.py
+++ scripts/create_hotspot_cfg.py
@@ -1,18 +1,15 @@
-# python program to create hotspot.cfg from hotspot-config-defn.json
+# Create hotspot.cfg from hotspot-config-defn.json current values
+#
+#   1 December 2023 - E M Thornber
+#   Created
+#
 
 import json
+import sys
 
-# Open JSON file
-f = open('./config/hotspot-config-defn.json')
+# Read JSON data and return JSON object as a dictionary
+data = json.load(sys.stdin)
 
-# returns JSON object as a dictionary
-data = json.load(f)
-
-# print(list(data.values()))
-
-# Interating through the json list
+# Interate through the json list and output to sys.stdout
 for (k, v) in iter(data.items()):
     print(k, '="', v.get('current'), '"', sep='', end='\n')
-
-# Close the file
-f.close()
